31f1da6740bb2d86595d394b6c06a306b50acfe5
joeymadhu7 <joeymadhu7@gmail.com>
2026-05-04 22:50:19 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.getenv("TOKEN")
+
+STARTING_PURSE = 200
+MAX_PLAYERS = 8
+BID_TIMEOUT = 15
+
+ADMIN_IDS = [8212194710, 8548675437, 7751493709]
